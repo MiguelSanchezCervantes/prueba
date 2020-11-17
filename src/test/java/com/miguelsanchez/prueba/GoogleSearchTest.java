a584caecce2ab6edf5784570241de612699abb7c
Miguel Sanchez Cervantes <miguel.san.cerv@gmail.com>
2020-11-17 15:00:49 -0600

    automated google search

---
+++ src/test/java/com/miguelsanchez/prueba/GoogleSearchTest.java
@@ -0,0 +1,45 @@
+package com.miguelsanchez.prueba;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GoogleSearchTest {
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		
+		System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver/geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	}
+	
+	
+	@Test
+	public void testGooglePage() throws InterruptedException {
+		WebElement searchbox = driver.findElement(By.name("q"));
+		searchbox.clear();
+		searchbox.sendKeys("Oracle");
+		searchbox.submit();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.titleIs("Oracle - Buscar con Google"));
+		assertEquals("Oracle - Buscar con Google", driver.getTitle());
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+}
